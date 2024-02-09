7078b3d1caba3c5694c5367f023b91546c722a51
Karina <me@karina.codes>
2024-02-09 03:48:14 -0600

    feat: use solid_cache for caching

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,8 @@ module MicroTube
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
+    config.cache_store = :solid_cache_store
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
