4f195aceda318e137b2b988f950bc93de62378ee
Karina <me@karina.codes>
2024-02-09 04:03:09 -0600

    feat: install devise

--- Gemfile
+++ Gemfile
@@ -16,10 +16,12 @@ gem "solid_cache"
 gem "redis", ">= 4.0.1"
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
-# gem "bcrypt", "~> 3.1.7"
+gem "bcrypt", "~> 3.1.7"
 
 gem "tzinfo-data", platforms: %i[ windows jruby ]
 gem "bootsnap", require: false
+
+gem "devise"
 
 group :test do
   gem "rspec-rails"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "micro_tube_production"
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 } # todo: set this to real values
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
